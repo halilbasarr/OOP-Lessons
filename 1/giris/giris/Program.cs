1da48c5cab262aa6f5f5b73eb79ff3709b2f3b7e
halilbasarr <hall.basrr@gmail.com>
2026-03-17 22:47:00 +0300

    Proje dosyası ekle.

---
+++ 1/giris/giris/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace giris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rastgele = new Random();
+
+            int hedefSayi = rastgele.Next(1,101);
+            int hak = 10;
+
+            Console.WriteLine("1 ile 100 arasındaki sayıyı bakalım bilecek misin?");
+
+            while (hak > 0)
+            {
+                Console.WriteLine($"\nKalan hakkın:{hak}. Tahminin: ");
+                string giris = Console.ReadLine();
+
+                if (int.TryParse(giris, out int tahmin))
+                {
+                    if (tahmin == hedefSayi)
+                    {
+                        Console.WriteLine("Tebrikler! Doğru bildin.");
+                    }
+                    else if (tahmin != hedefSayi)
+                    {
+                        Console.WriteLine("Bir daha dene.");
+                    }
+
+                    hak--;
+                }
+                else
+                {
+                    Console.WriteLine("Lütfen geçerli bir sayı giriniz.");
+                }
+            }
+
+            if (hak == 0) Console.WriteLine($"Üzgünüm, kaybettin! Bilmen gerek sayı buydu: {hedefSayi}");
+        }
+    }
+}
---
+++ metot/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan bilgi istiyoruz
+            Console.Write("Lütfen karesini almak istediğiniz sayıyı girin: ");
+            string girilenDeger = Console.ReadLine();
+
+            // Girilen değerin geçerli bir sayı olup olmadığını kontrol ediyoruz (ondalıklı sayılar için double)
+            if (double.TryParse(girilenDeger, out double sayi))
+            {
+                // İşlemi yapan metodu çağırıyoruz ve dönen değeri 'sonuc' değişkenine atıyoruz
+                double sonuc = KareAl(sayi);
+
+                // Sonucu ekrana yazdırıyoruz
+                Console.WriteLine($"{sayi} sayısının karesi: {sonuc}");
+            }
+            else
+            {
+                // Kullanıcı sayı dışında bir karakter girerse uyarı veriyoruz
+                Console.WriteLine("Hata: Lütfen geçerli bir sayı giriniz!");
+            }
+
+            // Ekranın hemen kapanmaması için bir tuşa basılmasını bekliyoruz
+            Console.WriteLine("\nÇıkmak için Enter'a basın...");
+            Console.ReadLine();
+        }
+        static double KareAl(double sayi)
+        {
+            return sayi * sayi;
+        }
+    }
+}
